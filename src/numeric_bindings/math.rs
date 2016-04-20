f496200c0b50b22f89ac47ead115bb76cf9400a0
Ian Longshore <lonian@seas.upenn.edu>
2016-04-20 19:37:51 -0400

    Now with documentation.  I'll generate and submit rustdocs in a minute.

--- src/numeric_bindings/math.rs
+++ src/numeric_bindings/math.rs
@@ -1,7 +1,14 @@
 use numeric::math;
 use numeric::tensor::Tensor;
 
+///In-place element-wise addition of the first argument.
 #[no_mangle]
-pub extern "C" fn log_64(x: Tensor<f64>, base: f64) -> Tensor<f64> {
-  math::log(x, base)
+pub extern "C" fn add_64(x: Tensor<f64>, y: &Tensor<f64>) -> Tensor<f64> {
+  x + y
+}
+
+///In-place dot-multiplication of the first argument.
+#[no_mangle]
+pub extern "C" fn dot_64(x: Tensor<f64>, y: &Tensor<f64>) -> Tensor<f64> {
+  x.dot(y)
 }
--- src/numeric_bindings/tensor.rs
+++ src/numeric_bindings/tensor.rs
@@ -4,20 +4,46 @@ use std::cmp::min;
 use std::ffi::CStr;
 use std::str::FromStr;
 
+/// Maybe this could just be a function?  I'm not sure.
 macro_rules! unsafe_take_cstr {
   ($literal:expr) => (unsafe { CStr::from_ptr($literal).to_string_lossy().into_owned() })
 }
 
+/// This is part of NumericRust's macro for making tensors.
+/// 
+/// This takes a string in the form of comma (',') delimited list of tokens 
+/// which can be parsed as floating point numbers.
 pub fn parse_row<T>(literal: &str) -> Result<Tensor<T>, T::Err>
   where T: FromStr + Copy { 
   let mut out = Vec::new();
   for tok in literal.split(",") {
-    out.push(try!(T::from_str(tok)));
+    out.push(try!(T::from_str(tok.trim())));
   }
   Ok(Tensor::new(out))
 }
 
-pub fn parse_columns<T>(literal: &str) -> Result<Tensor<T>, T::Err>
+/// This is the full functionality of NumericRust's macro for making tensors.
+/// I felt like just turning into a function which can be called on the Python
+/// side would be the easiest way to make tensor structs; the alternative would
+/// probably involve a function which takes in an array of pointers to arrays
+/// of doubles, or something, which would be harder to marshall than a single
+/// string with delimeters.
+/// 
+/// Each row in the output `Tensor` works the same way as it does in `parse_row`,
+/// except that rows of the tensor can be delimited by semicolons.
+/// 
+/// # Examples
+///
+/// ```
+/// parse_columns(&"7.0, 3.0, 2.0; -3.0, 2.0, -5.0")
+/// ```
+///
+/// This returns out a 2x3 matrix: 
+/// ```
+/// [ 7.0  3.0 -3.0 ]
+/// [-3.0  2.0 -5.0 ]
+/// ```
+pub fn parse_rows<T>(literal: &str) -> Result<Tensor<T>, T::Err>
   where T: FromStr + Copy {
   let mut out = Vec::new();
   let mut height = 0;
@@ -26,7 +52,7 @@ pub fn parse_columns<T>(literal: &str) -> Result<Tensor<T>, T::Err>
     height = height + 1;
     let mut width = 0;
     for tok in line.split(",") {
-      out.push(try!(T::from_str(tok)));
+      out.push(try!(T::from_str(tok.trim())));
       width = width + 1;
     }
     min_width = min(width, min_width);
@@ -34,14 +60,16 @@ pub fn parse_columns<T>(literal: &str) -> Result<Tensor<T>, T::Err>
   Ok(Tensor::new(out).reshape(&[min_width as isize, height as isize]))
 }
 
+///Exportable version of parse_row.  Possibly not necessary?
 #[no_mangle]
 pub extern "C" fn parse_tensor_1_64(literal: *const c_char) -> Option<Tensor<f64>> {
   let lit_from_c = unsafe_take_cstr!(literal);
   parse_row(&lit_from_c).ok()
 }
 
+///Exportable version of parse_rows.
 #[no_mangle]
 pub extern "C" fn parse_tensor_2_64(literal: *const c_char) -> Option<Tensor<f64>> {
   let lit_from_c = unsafe_take_cstr!(literal);
-  parse_columns(&lit_from_c).ok()
+  parse_rows(&lit_from_c).ok()
 }
