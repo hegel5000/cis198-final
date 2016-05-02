5018babd092f5e6131962acc167a2c565a4b3827
Parker Henry <parkerhenry0@gmail.com>
2016-05-02 06:15:30 -0400

    Implemented FFT

--- src/fft.rs
+++ src/fft.rs
@@ -7,9 +7,11 @@ use std::f64::consts::PI;
 /// of an f64 Vec with the Cooley-Turkey FFT algorithm
 /// (implementation taken from Wikipedia)
 ///
-pub fn fft(x: &Vec<Complex64>) -> &Vec<Complex64> {
+pub fn fft(x: &Vec<Complex64>) -> Vec<Complex64> {
+    let mut res: Vec<Complex64> = Vec::<Complex64>::with_capacity(x.len());
     if x.len() == 1 {
-        return x;
+        res.push(x[0]);
+        return res
     }
     else {
         // Follow the Cooley-Tukey recursive implementation
@@ -25,31 +27,42 @@ pub fn fft(x: &Vec<Complex64>) -> &Vec<Complex64> {
             .collect();
         let fft_1 = fft(x_1);
         let fft_2 = fft(x_2);
-        let mut fft = Vec::<Complex64>::with_capacity(x.len());
-        let k = 0;
+        let mut k = 0;
         while k < x.len() {
             if k % 2 == 0 {
-                fft.push(fft_1[k]);
+                res.push(fft_1[k/2]);
             } else {
-                fft.push(fft_2[k]);
+                res.push(fft_2[k/2]);
             }
+            k += 1;
         }
-        let k = 0;
+        let mut k = 0;
         let n = x.len();
         while k < n/2 - 1 {
-            let t = fft[k];
+            let t = res[k];
             let modulus: Complex64 = Complex::i() * 2.0 * PI * 
-                (k as c_double) / (n as c_double);
-            fft[k] = t + fft[k + n/2] * modulus.exp();
-            fft[k + n/2] = t - fft[k + n/2] * modulus.exp();
+                (((k as u32) / (n as u32)) as c_double);
+            res[k] = t + res[k + n/2] * modulus.exp();
+            res[k + n/2] = t - res[k + n/2] * modulus.exp();
+            k += 1;
         }
-        x
+        res
     }
 }
 
 #[cfg(test)]
 mod tests_fft {
+    use super::*;
+    use num::complex::Complex64;
+
     #[test]
     fn fft_compiles() {
+    }
+
+    #[test]
+    fn view_fft() {
+        let x: Vec<Complex64> = vec![1.0, 2.0, 3.0, 4.0, 5.0].iter()
+            .map(|x| Complex64::new(*x, 0.0)).collect();
+        println!("{:?}", fft(&x));
     }
 }
